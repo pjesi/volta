aea8884e004fe24fdad16f2aa000412aa78e6d8c
James.Bogosian <James.Bogosian@d1684418-87df-11dd-b6e4-1fb4a74862d0>
2008-10-30 15:41:54 +0000

    Resovles issue 16.  Removed the tabs around the editor, cleaned up the button bar, and added the Google Gadget button.

--- static/js/fck_config.js
+++ static/js/fck_config.js
@@ -1,19 +1,35 @@
+/*
+ * Copyright 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var pluginPath = '/static/js/plugins/';
+
 FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/silver/';
+FCKConfig.Plugins.Add('gadget', 'en', pluginPath);
 
 FCKConfig.ToolbarSets['AESC_Toolbar'] = [
-  ['FitWindow','Source','-','NewPage','Preview',],
-  ['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
-  ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-  '/',
-  ['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
-  ['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote'],
+  ['FitWindow','Source','-'],
+  ['Undo','Redo','-','Find','Replace'],
+  ['Bold','Italic','Underline','StrikeThrough'],
+  ['OrderedList','UnorderedList','-','Outdent','Indent'],
   ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-  ['Link','Unlink','Anchor'],
-  ['Image','Flash','Table','Rule','Smiley','SpecialChar','PageBreak'],
+  ['Link','Unlink'],
+  ['Gadget', 'Image','Flash','Table','SpecialChar'],
   '/',
   ['Style','FontFormat','FontName','FontSize'],
-  ['TextColor','BGColor'],
-  ['About'] // No comma for the last row.
+  ['TextColor','BGColor'] // No comma for the last row.
 ];
 
 FCKConfig.ImageBrowser = false;
